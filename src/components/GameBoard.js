1063b9d774e4433d9343321dda25908eae2f86e0
Zak Laughton <zlaughton@gmail.com>
2020-04-01 12:29:04 -0500

    lift up state to global

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,43 +1,8 @@
 import React, { useState } from 'react';
 import Person from './Person';
 import styled from 'styled-components';
-import { shuffleArray } from '../utils/utils';
 
-function GameBoard() {
-  const gridSize = 100;
-  const boardSize = 1000;
-  const cellSize = boardSize / gridSize;
-  const numberOfPeople = 500;
-
-  const generateAllPositions = () => {
-    let positionList = [];
-    for (let x = 0; x < gridSize; x++) {
-      for (let y = 0; y < gridSize; y++) {
-        positionList.push([x, y]);
-      }
-    }
-    return positionList;
-  };
-
-  const allPositions = generateAllPositions();
-  const generateRandomPeople = () => {
-    let shuffledLocations = shuffleArray(allPositions);
-    const people = shuffledLocations.slice(0, numberOfPeople).map(location => {
-      return { location };
-    });
-    return people;
-  };
-
-  const initialPeople = generateRandomPeople();
-
-  const [people, setPeople] = useState(initialPeople);
-  const movePeople = () => {
-    const newPeople = people.map(person => {
-      return { location: [person.location[0] + 5, person.location[1]] };
-    });
-    setPeople(newPeople);
-  };
-
+function GameBoard({ boardSize, movePeople, cellSize, people }) {
   return (
     <Board size={boardSize} onClick={movePeople}>
       {people.map((person, index) => (
